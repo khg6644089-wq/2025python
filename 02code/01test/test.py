f35b83fc619c2e5e6880478926f10697a644513a
hoons <khg6644089@hanmail.net>
2025-12-23 16:21:49 +0900

    1st

---
+++ 02code/01test/test.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt 
+
+# age = 100
+# # print(age)
+# # print(type(age))
+
+# type(age)
+
+x = [1, 2, 3, 4]
+y = [10, 20, 25, 30]
+
+#plt.plot(x, y)
+plt.bar(x, y)
+plt.show()
